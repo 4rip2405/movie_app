6a5ca393a07b99a70ba6e8cdd1f868a2da5cc5fe
4rif2405 <arifpaturrahman3@gmail.com>
2024-06-24 19:13:01 +0700

    develop home screen

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,32 +1,56 @@
-import React from 'react';
-import { SafeAreaView, View, Text, Button, StyleSheet } from 'react-native';
+import React from 'react'
+import { ScrollView, View, StatusBar, StyleSheet } from 'react-native'
+import type { MovieListProps } from '../types/app'
+import MovieList from '../components/movies/MovieList'
 
-export default function Home({ navigation }: any): JSX.Element {
+const movieLists: MovieListProps[] = [
+  {
+    title: 'Now Playing in Theater',
+    path: 'movie/now_playing?language=en-US&page=1',
+    coverType: 'backdrop',
+  },
+  {
+    title: 'Upcoming Movies',
+    path: 'movie/upcoming?language=en-US&page=1',
+    coverType: 'poster',
+  },
+  {
+    title: 'Top Rated Movies',
+    path: 'movie/top_rated?language=en-US&page=1',
+    coverType: 'poster',
+  },
+  {
+    title: 'Popular Movies',
+    path: 'movie/popular?language=en-US&page=1',
+    coverType: 'poster',
+  },
+]
+
+const Home = (): JSX.Element => {
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.content}>
-        <Text style={styles.title}>Home</Text>
-        <Button
-          title="Go to Movie Detail"
-          onPress={() => navigation.navigate('MovieDetail')}
-        />
+    <ScrollView>
+      <View style={styles.container}>
+        {movieLists.map((movieList) => (
+          <MovieList
+            title={movieList.title}
+            path={movieList.path}
+            coverType={movieList.coverType}
+            key={movieList.title}
+          />
+        ))}
+        <StatusBar translucent={false} />
       </View>
-    </SafeAreaView>
-  );
+    </ScrollView>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-  },
-  content: {
-    flex: 1,
-    justifyContent: 'center',
+    marginTop: StatusBar.currentHeight ?? 32,
     alignItems: 'center',
+    justifyContent: 'center',
+    rowGap: 16,
   },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 20,
-  },
-});
+})
+
+export default Home
