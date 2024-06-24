56733f364f9da09b4680ed57d9ba71568fe3930a
4rif2405 <arifpaturrahman3@gmail.com>
2024-06-24 14:27:01 +0700

    make an integrated API

--- src/screens/MovieDetail.tsx
+++ src/screens/MovieDetail.tsx
@@ -1,31 +1,42 @@
-import React from 'react';
-import { SafeAreaView, View, Text, Button, StyleSheet } from 'react-native';
+import React from 'react'
+import { View, Text, Button } from 'react-native'
 
-const MovieDetail = ({ navigation }: any): JSX.Element => {
+const MovieDetail = ({ navigation }: any): any => {
+  const fetchData = (): void => {
+    const ACCESS_TOKEN = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwNDRiZTY5NWZkOWY3MGVmNjc3ZWY3Yzk2YjUzMTI4NCIsIm5iZiI6MTcxOTIwODg4Ni40MDI5MzMsInN1YiI6IjY1ZWVmNGJhZjVjYjIxMDE4NTQ1YzhmNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.IwDYuR2oSO90OIJagHM9sSTg4iNNYdPKoMj0sr6byUE'
+
+    const url =
+      'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1'
+
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${ACCESS_TOKEN}`,
+      },
+    }
+
+    fetch(url, options)
+      .then(async (response) => await response.json())
+      .then((response) => {
+        console.log(response)
+      })
+      .catch((err) => {
+        console.error(err)
+      })
+  }
+
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.content}>
-        <Text style={styles.title}>Movie Detail Screen</Text>
-        <Button title="Go back" onPress={() => navigation.goBack()} />
-      </View>
-    </SafeAreaView>
-  );
-};
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Movie Detail Page</Text>
+      <Button
+        title="Fetch Data"
+        onPress={() => {
+          fetchData()
+        }}
+      />
+    </View>
+  )
+}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  content: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 20,
-  },
-});
-
-export default MovieDetail;
+export default MovieDetail
